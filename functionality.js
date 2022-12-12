56abbf02685311acf69f640ebea5fe6222a80ad2
Harry Whitelam <gfs19eku@UEA.AC.UK>
2022-12-12 14:09:23 +0000

    dropdown working needs some extra generalisation/future-proofing

--- functionality.js
+++ functionality.js
@@ -2,6 +2,15 @@ const NEWSCI = ['0.01', '0.02', '0.03', '0.04', '0.05', '0.06', '0.07', '0.08', 
 const TPSC = ['0.01', '0.02', '0.03', '0.04', '0.05', '0.06', '0.07', '0.08', '0.09'];
 const JSC = ['0.01', '0.02', '0.03', '0.04', '0.05', '0.06', '0.07', '0.08', '0.09'];
 
+//const STAFF = ['buildingInput', 'roomInput', 'typeInput', 'dateInput', 'searchButton', 'clearButton'];
+const STAFF = ['floorInput', 'faultIdInput'];
+const STUDENT = ['floorInput', 'faultIdInput'];
+const ENGINEER = [];
+const ADMIN = [];
+let actors = [STAFF, STUDENT, ENGINEER, ADMIN];
+let currentActor = 0;
+
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
@@ -9,6 +18,44 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+//Display functions
+function swapDisplay() {
+  if (currentActor !== 3) {
+    currentActor++;
+  }
+  else {
+    currentActor = 0;
+  }
+  searchDisplay(currentActor);
+}
+
+function searchDisplay(actorType) {
+  const inputs = document.getElementsByTagName("input");
+  const actor = actors.at(actorType);
+  Array.from(inputs).forEach(input => {
+    input.style.display = 'inline';
+    document.getElementById(input.id+"Label").style.display = 'inline';
+    actor.forEach(actorField => {
+      if (actorField === input.id) {
+        input.style.display = 'none';
+        document.getElementById(input.id+"Label").style.display = 'none';
+      }
+    })
+  });
+}
+
+function dropDown() {
+  const row = document.getElementById("firstDetails");
+  if (row.style.display !== 'none') {
+    row.style.display = 'none';
+  }
+  else {
+    row.style.display = 'inline-block';
+  }
+}
+
+
 
 //FaultSearch
 function clearSearchForm() {
